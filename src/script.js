f796fbff3a2ecb4dd284a4df91477063b44b70f7
yichien <kevinyichienlee@gmail.com>
2022-05-27 22:56:32 +0800

    added generateGalaxy func and random radius

--- src/script.js
+++ src/script.js
@@ -7,7 +7,7 @@ import * as dat from 'lil-gui'
  * Base
  */
 // Debug
-const gui = new dat.GUI()
+const gui = new dat.GUI({ width: 400})
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -15,14 +15,68 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * Test cube
- */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
-scene.add(cube)
+// Galaxy
+const parameters = {}
+parameters.count = 10000;
+parameters.size = 0.01;
+parameters.radius = 5
+
+let geometry = null
+let material = null
+let points = null
+
+
+const generateGalaxy = () =>  {
+
+
+      // Destroy old galaxy
+      if(points !== null)
+      {
+          geometry.dispose()
+          material.dispose()
+          scene.remove(points)
+      }
+    /**
+     * Geometry
+    */ 
+    geometry = new THREE.BufferGeometry()
+
+    const positions = new Float32Array(parameters.count * 3)
+
+    for (let i = 0; i < parameters.count; i++) {
+
+        const i3 = i * 3
+
+        const radius = Math.random() * parameters.radius
+
+        positions[i3 + 0] = radius
+        positions[i3 + 1] = 0
+        positions[i3 + 2] = 0
+    }
+
+    geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+    /**
+     * Material
+    */ 
+    material = new THREE.PointsMaterial({
+        size: parameters.size,
+        sizeAttenuation: true,
+        depthWrite: false,
+        blending: THREE.AdditiveBlending
+    })
+
+    /**
+     * Points
+     */
+    points = new THREE.Points(geometry, material)
+    scene.add(points)
+
+}
+
+gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy)
+gui.add(parameters, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy)
+gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
+generateGalaxy()
 
 /**
  * Sizes
