ce6ab136b2b016a358de29bbad343c2aaf103ea8
yichien <kevinyichienlee@gmail.com>
2022-05-28 23:28:39 +0800

    remove spin formula and its gui

--- src/script.js
+++ src/script.js
@@ -17,12 +17,12 @@ const scene = new THREE.Scene()
 
 // Galaxy
 const parameters = {}
-parameters.count = 100000;
-parameters.size = 0.01;
+parameters.count = 200000;
+parameters.size = 0.005;
 parameters.radius = 5;
 parameters.branches = 3;
 parameters.spin = 1;
-parameters.randomness = 0.2;
+parameters.randomness = 0.5;
 parameters.randomnessPower = 3
 parameters.insideColor = '#ff6030'
 parameters.outsideColor = '#1b3984'
@@ -58,7 +58,7 @@ const generateGalaxy = () =>  {
         const i3 = i * 3
 
         const radius = Math.random() * parameters.radius
-        const spinAngle = radius * parameters.spin
+        
         const branchAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2 // 20 % 3 / (3 let the points not really on the branch) 
         
 
@@ -67,9 +67,9 @@ const generateGalaxy = () =>  {
         const randomY = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
         const randomZ = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
 
-        positions[i3    ] = Math.cos(branchAngle + spinAngle) * radius + randomX
+        positions[i3    ] = Math.cos(branchAngle) * radius + randomX
         positions[i3 + 1] = randomY
-        positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius + randomZ
+        positions[i3 + 2] = Math.sin(branchAngle) * radius + randomZ
 
         // Color
         const mixedColor = colorInside.clone() // not change the base colorInside
@@ -104,10 +104,9 @@ const generateGalaxy = () =>  {
 }
 
 gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy)
-gui.add(parameters, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy)
 gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
 gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
-gui.add(parameters, 'spin').min(-5).max(5).step(0.001).onFinishChange(generateGalaxy)
+
 gui.add(parameters, 'randomness').min(0).max(2).step(0.001).onFinishChange(generateGalaxy)
 gui.add(parameters, 'randomnessPower').min(1).max(10).step(0.001).onFinishChange(generateGalaxy)
 gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy)
