007c5a4278076a1f702f6e00b7dd42569346971a
yichien <kevinyichienlee@gmail.com>
2022-05-28 23:56:22 +0800

    Move the shaders to separate files

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -76,7 +76,18 @@ module.exports = {
                 {
                     filename: 'assets/fonts/[hash][ext]'
                 }
+            },
+
+            // Shaders
+            {
+                test: /\.(glsl|vs|fs|vert|frag)$/,
+                type: 'asset/source',
+                generator:
+                {
+                    filename: 'assets/images/[hash][ext]'
+                }
             }
+
         ]
     }
 }
