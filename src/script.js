0a3def460849f335f6f83cad2d585c9969ba4c43
yichien <kevinyichienlee@gmail.com>
2022-05-29 16:57:14 +0800

    added bg

--- src/script.js
+++ src/script.js
@@ -26,6 +26,52 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+/**
+ * Textures
+ */
+ const textureLoader = new THREE.TextureLoader()
+ const particleTexture = textureLoader.load('/textures/particles/4.png')
+
+ /**
+ * Particles
+ */
+// Geometry
+const particlesGeometry = new THREE.BufferGeometry()
+const count = 50000
+
+const positions = new Float32Array(count * 3)
+const colors = new Float32Array(count * 3)
+
+for(let i = 0; i < count * 3; i++)
+{
+    positions[i] = (Math.random() - 0.5) * 10
+    colors[i] = Math.random()
+}
+
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+particlesGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
+
+// Material
+const particlesMaterial = new THREE.PointsMaterial()
+
+particlesMaterial.size = 0.1
+particlesMaterial.sizeAttenuation = true
+
+particlesMaterial.color = new THREE.Color('#ff88cc')
+
+particlesMaterial.transparent = true
+particlesMaterial.alphaMap = particleTexture
+// particlesMaterial.alphaTest = 0.01
+// particlesMaterial.depthTest = false
+particlesMaterial.depthWrite = false
+particlesMaterial.blending = THREE.AdditiveBlending
+
+particlesMaterial.vertexColors = true
+
+// Points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
+
 // Galaxy
 const parameters = {}
 parameters.count = 200000;
@@ -200,7 +246,7 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
-
+    particles.rotation.y =  - elapsedTime * 0.02
     // Update material
     material.uniforms.uTime.value = elapsedTime
 
