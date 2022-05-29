6fb338dbfed02a22357ae3a4818b182e08c10953
yichien <kevinyichienlee@gmail.com>
2022-05-29 16:09:53 +0800

    added color

--- src/shaders/galaxy/fragment.glsl
+++ src/shaders/galaxy/fragment.glsl
@@ -1,3 +1,5 @@
+varying vec3 vColor;
+
 void main()
 {
      // Light point
@@ -6,4 +8,8 @@ void main()
     strength = pow(strength, 10.0);
 
     gl_FragColor = vec4(vec3(strength), 1.0);
+
+     // Final color
+    vec3 color = mix(vec3(0.0), vColor, strength);
+    gl_FragColor = vec4(color, 1.0);
 }
