b26a7049374993d7c04be59db9e3295790534258
yichien <kevinyichienlee@gmail.com>
2022-05-29 15:50:46 +0800

    light point pattern and increase size

--- src/script.js
+++ src/script.js
@@ -103,7 +103,7 @@ const generateGalaxy = () =>  {
         vertexShader: galaxyVertexShader,
         fragmentShader: galaxyFragmentShader,
         uniforms: {
-            uSize: { value: 8 * renderer.getPixelRatio()}
+            uSize: { value: 30 * renderer.getPixelRatio()}
         }
     })
 
