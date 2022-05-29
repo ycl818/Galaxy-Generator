4a89c4d334e164aab53ce3111a2bb8efa8631d71
yichien <kevinyichienlee@gmail.com>
2022-05-29 16:40:24 +0800

    removed gui and added dbclick

--- src/script.js
+++ src/script.js
@@ -1,17 +1,24 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'lil-gui'
+//import * as dat from 'lil-gui'
 
 import galaxyVertexShader from './shaders/galaxy/vertex.glsl'
 import galaxyFragmentShader from './shaders/galaxy/fragment.glsl'
 
-console.log(galaxyFragmentShader);
+window.addEventListener('dblclick', () => {
+    if (!document.fullscreenElement) {
+        canvas.requestFullscreen()
+    } else {
+        document.exitFullscreen()
+    }
+})
+
 /**
  * Base
  */
 // Debug
-const gui = new dat.GUI({ width: 400})
+//const gui = new dat.GUI({ width: 400})
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -122,14 +129,14 @@ const generateGalaxy = () =>  {
 
 }
 
-gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy)
-gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
-gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
+// gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy)
+// gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
+// gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
 
-gui.add(parameters, 'randomness').min(0).max(2).step(0.001).onFinishChange(generateGalaxy)
-gui.add(parameters, 'randomnessPower').min(1).max(10).step(0.001).onFinishChange(generateGalaxy)
-gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy)
-gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy)
+// gui.add(parameters, 'randomness').min(0).max(2).step(0.001).onFinishChange(generateGalaxy)
+// gui.add(parameters, 'randomnessPower').min(1).max(10).step(0.001).onFinishChange(generateGalaxy)
+// gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy)
+// gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy)
 
 
 
